f6d212b5ab5630b9c7e3da6b06048cc609a5bfef
wjd-java <929155002@qq.com>
2016-10-31 11:58:28 +0800

    添加分页pojo类

---
+++ hotel-common/src/main/java/com/wjd/common/pojo/PagingQuery.java
@@ -0,0 +1,20 @@
+package com.wjd.common.pojo;
+
+public class PagingQuery {
+	private int nums;
+	private int rows;
+	public int getNums() {
+		return nums;
+	}
+	public void setNums(int nums) {
+		this.nums = nums;
+	}
+	public int getRows() {
+		return rows;
+	}
+	public void setRows(int rows) {
+		this.rows = rows; 
+	}
+	
+	
+}
